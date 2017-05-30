2555d7bbc5f1f9eb8512f8b699e3c1b8b19cd6dd
kms <kms@TIMPC-508>
2017-05-30 21:00:44 +0400

    Add project files.

---
+++ WindowsFormsScreenSnoop/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsScreenSnoop
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Construct an image object from a file in the local directory.
+            // ... This file must exist in the solution.
+            Image image = Image.FromFile("f:/Projects/a.jpg");
+            // Set the PictureBox image property to this image.
+            // ... Then, adjust its height and width properties.
+            pictureBox1.Image = image;
+            pictureBox1.Height = image.Height;
+            pictureBox1.Width = image.Width;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
